5cc91d3089582451032e6d8ddf6f9adf049bbf6b
Shaff Kazi <shaffkazi98@gmail.com>
2023-03-21 20:32:03 +0400

    Final Submission

---
+++ subjects.js
@@ -0,0 +1,61 @@
+let subjects = [{
+        "id": 101,
+        // "image": "img/FlyDubai.png",
+        "subject": 'Laser Pointer',
+        "location": 'Sweden',
+        "price": 220,
+        "Spaces": 'this is a Laser pointer from Sweden',
+        // "availableInventory": 8,
+        // "rating": 2
+    }
+
+    // {
+
+    //     "id": 202,
+    //     "image": 'images/math symbol.png',
+    //     "subject": 'blue pointer',
+    //     "location": 'Sweden',
+    //     "price": 220,
+    //     "detail": 'this is a blue pointer from Sweden',
+    //     "availableInventory": 8,
+    //     "rating": 3
+    // },
+
+    // {
+
+    //     "id": 303,
+    //     "image": 'images/math symbol.png',
+    //     "subject": 'red pointer',
+    //     "location": 'Sweden',
+    //     "price": 220,
+    //     "detail": 'this is a red pointer from Sweden',
+    //     "availableInventory": 8,
+    //     "rating": 4
+    // },
+
+    // {
+
+    //     "id": '404',
+    //     "image": 'images/math symbol.png',
+    //     "subject": 'yellow pointer',
+    //     "location": 'Sweden',
+    //     "price": 220,
+    //     "detail": 'this is a yellow pointer from Sweden',
+    //     "availableInventory": 8,
+    //     "rating": 2
+    // },
+
+    // {
+
+    //     "id": 505,
+    //     "image": 'images/math symbol.png',
+    //     "subject": 'black pointer',
+    //     "location": 'Sweden',
+    //     "price": 220,
+    //     "detail": 'this is a black pointer from Sweden',
+    //     "availableInventory": 8,
+    //     "rating": 5
+    // }
+
+
+]
